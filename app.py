daa2e16d8aa3dd9ba708e47f60af0a449e3baae5
Blake Richards <richards3284@gmail.com>
2018-11-10 14:33:02 -0500

    code cleanup, added requestdatasets part of api

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ def MakeOptions(appendString, validKeys, requiredKeys, options):
             else:
                 print('{} is not a valid option!'.format(key))
     else:
-
-        appendString = 'Missing required arguments (' + ', '.join([missingKey for missingKey in requiredKeys if missingKey not in list(options.keys())]) \
+        appendString = 'Missing required arguments (' + \
+                       ', '.join([missingKey for missingKey in requiredKeys if missingKey not in list(options.keys())]) \
                        + ') for \"' + appendString + '\" API call.'
     return appendString
 
@@ -34,35 +34,40 @@ def RequestData(**kwargs):
                  'enddate', 'units', 'sortfield', 'sortorder', 'limit', 'offset',
                  'includemetadata']
     requiredKeys = ['datasetid', 'startdate', 'enddate']
-    return  MakeOptions('data', validKeys, requiredKeys,  kwargs)
+    return MakeOptions('data', validKeys, requiredKeys,  kwargs)
 
 def RequestDataCategories(**kwargs):
     validKeys = ['datasetid', 'locationid', 'stationid', 'startdate', 'enddate',
                  'sortfield', 'sortorder', 'limit', 'offset']
-    appendString = "datacategories"
-    for key,value in kwargs.items():
-        if key in validKeys:
-            appendString = appendString + "?{}={}".format(key,value)
-        else:
-            print('{} is not a valid option!'.format(key)) 
-    return appendString
+    requiredKeys = []
+    return MakeOptions('datacategories', validKeys, requiredKeys, kwargs)
+
+def RequestDatasets(**kwargs):
+    validKeys = ['datatypeid', 'locationid', 'stationid', 'startdate', 'enddate',
+                 'sortfield', 'sortorder', 'limit', 'offset']
+    requiredKeys = []
+    return MakeOptions('datasets', validKeys, requiredKeys, kwargs)
 
 def main(accessToken):
     baseURL = "https://www.ncdc.noaa.gov/cdo-web/api/v2/"
     # Example 
     # response = requests.get(baseURL + "stations",headers={'token': accessToken})
     # print(response.text)
-    #response = requests.get(baseURL + "stations?locationid=FIPS:51", headers={'token': accessToken})
-    #response = requests.get(baseURL + "stations?locationid=FIPS:51", headers={'token': accessToken})
-    #response = requests.get(baseURL + "locationcategories?limit=1000", headers={'token': accessToken}).json()
+    # response = requests.get(baseURL + "stations?locationid=FIPS:51", headers={'token': accessToken})
+    # response = requests.get(baseURL + "stations?locationid=FIPS:51", headers={'token': accessToken})
+    # response = requests.get(baseURL + "locationcategories?limit=1000", headers={'token': accessToken}).json()
 #    response = requests.get(baseURL + "datacategories?limit=1000", headers={'token': accessToken}).json()
 #    for i in range(len(response['results'])): print(response['results'][i]['name'])
-    strRequest = RequestData(limit="1000",testKey='999')
+    #strRequest = RequestData(limit="1000",testKey='999')
+    # strRequest = RequestDataCategories(limit='1000')
+    strRequest = RequestDatasets(limit='1000')
     if 'Missing required arguments' not in strRequest:
         response = requests.get(baseURL + strRequest, headers={'token': accessToken})
         print(response.text)
         jResponse = response.json()
-        #for i in range(len(jResponse['results'])): print(jResponse['results'][i]['name'])
+        for i in range(len(jResponse['results'])):
+            print(jResponse['results'][i]['name'] + ' : ', end='')
+            print(jResponse['results'][i]['id'])
     else:
         print(strRequest)
 
