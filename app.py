1c24798e8c6a8ed61578d9e29a4ee66581f88e87
Blake Richards <richards3284@gmail.com>
2018-11-10 14:12:01 -0500

    Added better output based on whats missing in API call, and prettier config loading outputs

--- app.py
+++ app.py
@@ -2,15 +2,16 @@ import requests
 import os.path
 
 def GetAccessToken():
+    print('Reading configuration settings..', end='')
     if os.path.isfile('conf.ini'):
-        print('Reading configuration settings..')
         with open('conf.ini', 'r') as confFile:
             for line in confFile.readlines():
                 confSetting = line.replace(' ', '').rstrip('\n\r').replace('{', '').replace('}', '').split(":")
                 if confSetting[0] == 'accessToken':
+                    print('..Access token found!')
                     return confSetting[1]
     else:
-        print('No configuration found! Creating conf.ini.')
+        print('..No configuration found! Creating conf.ini.')
         with open('conf.ini', 'w') as confFile:
             confFile.write('{accessToken} : {}')
     return ""
@@ -23,17 +24,21 @@ def MakeOptions(appendString, validKeys, requiredKeys, options):
             else:
                 print('{} is not a valid option!'.format(key))
     else:
-        appendString = 'Missing required args'
+
+        appendString = 'Missing required arguments (' + ', '.join([missingKey for missingKey in requiredKeys if missingKey not in list(options.keys())]) \
+                       + ') for \"' + appendString + '\" API call.'
     return appendString
 
 def RequestData(**kwargs):
-    validKeys = ['datasetid', 'datatypeid', 'locationid', 'stationid', 'startdate', 'enddate', 'units', 
-                'sortfield', 'sortorder', 'limit', 'offset','includemetadata']
-    requiredKeys = ['datasetid','startdate','enddate']
+    validKeys = ['datasetid', 'datatypeid', 'locationid', 'stationid', 'startdate',
+                 'enddate', 'units', 'sortfield', 'sortorder', 'limit', 'offset',
+                 'includemetadata']
+    requiredKeys = ['datasetid', 'startdate', 'enddate']
     return  MakeOptions('data', validKeys, requiredKeys,  kwargs)
 
 def RequestDataCategories(**kwargs):
-    validKeys = ['datasetid', 'locationid', 'stationid', 'startdate', 'enddate', 'sortfield', 'sortorder', 'limit', 'offset']
+    validKeys = ['datasetid', 'locationid', 'stationid', 'startdate', 'enddate',
+                 'sortfield', 'sortorder', 'limit', 'offset']
     appendString = "datacategories"
     for key,value in kwargs.items():
         if key in validKeys:
@@ -53,7 +58,7 @@ def main(accessToken):
 #    response = requests.get(baseURL + "datacategories?limit=1000", headers={'token': accessToken}).json()
 #    for i in range(len(response['results'])): print(response['results'][i]['name'])
     strRequest = RequestData(limit="1000",testKey='999')
-    if strRequest != 'Missing required args':
+    if 'Missing required arguments' not in strRequest:
         response = requests.get(baseURL + strRequest, headers={'token': accessToken})
         print(response.text)
         jResponse = response.json()
